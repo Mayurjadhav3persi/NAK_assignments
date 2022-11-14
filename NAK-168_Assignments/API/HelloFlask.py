ff9a81731a102e48b8b9a75e0b6926349adc51f7
Mayurjadhav3persi <115109899+Mayurjadhav3persi@users.noreply.github.com>
2022-11-14 10:57:36 +0530

    Update HelloFlask.py

--- NAK-168_Assignments/API/HelloFlask.py
+++ NAK-168_Assignments/API/HelloFlask.py
@@ -7,18 +7,21 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+with app.app_context():
+    class Todo(db.Model):
+        id = db.Column(db.Integer, primary_key=True )
+        title = db.Column(db.String(100))
+        complete = db.Column(db.Boolean)
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
-    complete = db.Column(db.Boolean)
+    db.create_all()
 
 
-@app.route("/")
-def home():
+
+@app.route('/')
+def index():
     todo_list = Todo.query.all()
-    return render_template("base.html", todo_list=todo_list)
-
+    print(todo_list)
+    return render_template('base.html', todo_list=todo_list)
 
 @app.route("/add", methods=["POST"])
 def add():
@@ -26,27 +29,22 @@ def add():
     new_todo = Todo(title=title, complete=False)
     db.session.add(new_todo)
     db.session.commit()
-    return redirect(url_for("home"))
-
+    return redirect(url_for("index"))
 
 @app.route("/update/<int:todo_id>")
 def update(todo_id):
     todo = Todo.query.filter_by(id=todo_id).first()
     todo.complete = not todo.complete
     db.session.commit()
-    return redirect(url_for("home"))
-
+    return redirect(url_for("index"))
 
 @app.route("/delete/<int:todo_id>")
 def delete(todo_id):
     todo = Todo.query.filter_by(id=todo_id).first()
     db.session.delete(todo)
     db.session.commit()
-    return redirect(url_for("home"))
+    return redirect(url_for("index"))
 
-if __name__ == "__main__":
-    db.create_all()
-    new_todo = todo(titile="todo 1", complete=False)
-    db.session.add(new_todo)
-    db.session.commit()
+if __name__=="__main__":
+    
     app.run(debug=True)
